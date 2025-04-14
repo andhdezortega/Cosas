e059a18992a71a12541572f5ec0110e35a4b321f
andhdezortega <andhdezortega@gmail.com>
2025-04-14 07:09:19 +0200

    2.Recorrer Array

---
+++ RecorrerArray.java
@@ -0,0 +1,21 @@
+
+public class RecorrerArray {
+
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+        
+        
+        //bucle tradicional
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+        
+       /* //bucle for each
+        System.out.println("recorrido forich");
+        for (int numero : numeros){
+            System.out.println(numero);
+        }
+*/
+    }
+    
+}
